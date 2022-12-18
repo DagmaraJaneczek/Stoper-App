da38657ac3afd5deafb1ccdc47b44b6daee12e3f
DagmaraJaneczek <janeczek.dagmara@gmail.com>
2022-12-18 15:36:06 +0100

    Remove files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(null);
+
+/* Add info to the state */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button>Start</button>
+      <button>Stop</button>
+      <button>Reset</button>
     </div>
   );
-}
+};
 
 export default App;
